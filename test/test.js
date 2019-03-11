5421a55c371366782b815406f635ef2d5750a575
jin5354 <xiaoyanjinx@gmail.com>
2019-03-11 21:40:37 +0800

    [fix] fix test

--- test/test.js
+++ test/test.js
@@ -22,47 +22,51 @@ test('wrapped axios feature basis test', async t => {
   let http = wrapper(axios)
 
   let response1 = await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
 
   let response2 = await http({
-    url: 'http://www.404forest.com:3000/users/jin5354'
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577'
   })
 
-  let response3 = await http.get('http://www.404forest.com:3000/users/jin5354')
+  let response3 = await http.get('http://dummy.restapiexample.com/api/v1/employee/7577')
 
   let response4 = await http({
-    url: 'http://www.404forest.com:3000/users/yyhappynice',
+    url: 'http://dummy.restapiexample.com/api/v1/create',
     method: 'post',
     data: {
-      id: 10
+      name: 'test' + Date.now(),
+      salary: '123',
+      age: 23
     }
   })
 
-  let response5 = await http.get('http://www.404forest.com:3000/users/jin5354', {
+  let response5 = await http.get('http://dummy.restapiexample.com/api/v1/employee/7577', {
     params: {
       ID: 'somethingelse'
     }
   })
 
-  let response6 = await http.post('http://www.404forest.com:3000/users/llissery', {
-    id: 11
+  let response6 = await http.post('http://dummy.restapiexample.com/api/v1/create', {
+    name: 'test' + Date.now(),
+    salary: '13',
+    age: 23
   })
 
-  let response7 = await http.get('http://www.404forest.com:3000/users/llissery', {
+  let response7 = await http.get('http://dummy.restapiexample.com/api/v1/employee/7578', {
     params: {
       ID: 'somethingelse'
     }
   }, 'needless fault-tolerance param')
 
-  t.is(response1.data.id, 6868950)
-  t.is(response2.data.id, 6868950)
-  t.is(response3.data.id, 6868950)
-  t.is(response4.data.id, 10)
-  t.is(response5.data.id, 6868950)
-  t.is(response6.data.id, 11)
-  t.is(response7.data.id, 11)
+  t.is(response1.data.id, '7577')
+  t.is(response2.data.id, '7577')
+  t.is(response3.data.id, '7577')
+  t.is(response4.data.salary, '123')
+  t.is(response5.data.id, '7577')
+  t.is(response6.data.salary, '13')
+  t.is(response7.data.id, '7578')
 })
 
 test('wrapped axios instance feature basis test', async t => {
@@ -75,25 +79,25 @@ test('wrapped axios instance feature basis test', async t => {
   let http = wrapper(instance)
 
   let response1 = await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
 
   let response2 = await http({
-    url: 'http://www.404forest.com:3000/users/jin5354'
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577'
   })
 
-  let response3 = await http.get('http://www.404forest.com:3000/users/jin5354')
+  let response3 = await http.get('http://dummy.restapiexample.com/api/v1/employee/7577')
 
-  t.is(response1.data.id, 6868950)
-  t.is(response2.data.id, 6868950)
-  t.is(response3.data.id, 6868950)
+  t.is(response1.data.id, '7577')
+  t.is(response2.data.id, '7577')
+  t.is(response3.data.id, '7577')
 })
 
 test('addFilter and removeFilter test', async t => {
 
   let http = wrapper(axios)
-  let reg = /users/
+  let reg = /employee/
   t.is(http.__cacher.filters.length, 0)
   http.__addFilter(reg)
 
@@ -102,7 +106,7 @@ test('addFilter and removeFilter test', async t => {
   t.is(http.__cacher.cacheMap.size, 0)
 
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
 
@@ -113,7 +117,7 @@ test('addFilter and removeFilter test', async t => {
   t.is(http.__cacher.filters.length, 0)
 
   await http({
-    url: 'http://www.404forest.com:3000/users/yyhappynice',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7578',
     method: 'get'
   })
 
@@ -125,31 +129,31 @@ test('maxCacheSize test', async t => {
   let http = wrapper(axios, {
     maxCacheSize: 3
   })
-  let reg = /users/
+  let reg = /employee/
   http.__addFilter(reg)
 
   t.is(http.__cacher.maxCacheSize, 3)
   t.is(http.__cacher.cacheMap.size, 0)
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
   await http({
-    url: 'http://www.404forest.com:3000/users/yyhappynice',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7578',
     method: 'get'
   })
   await http({
-    url: 'http://www.404forest.com:3000/users/llissery',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7579',
     method: 'get'
   })
   t.is(http.__cacher.cacheMap.size, 3)
   await http({
-    url: 'http://www.404forest.com:3000/users/IOriens',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7580',
     method: 'get'
   })
   t.is(http.__cacher.cacheMap.size, 3)
   t.falsy(http.__cacher.hasCache({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   }))
 
@@ -160,25 +164,25 @@ test('ttl test', async t => {
   let http = wrapper(axios, {
     ttl: 1000
   })
-  let reg = /users/
+  let reg = /employee/
   http.__addFilter(reg)
 
   t.is(http.__cacher.ttl, 1000)
   t.is(http.__cacher.cacheMap.size, 0)
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
   t.is(http.__cacher.cacheMap.size, 1)
   t.truthy(http.__cacher.hasCache({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   }))
 
   await pause(1100)
 
   t.falsy(http.__cacher.hasCache({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   }))
 
@@ -187,22 +191,22 @@ test('ttl test', async t => {
 test('hit cache test', async t => {
 
   let http = wrapper(axios)
-  let reg = /users/
+  let reg = /employee/
   http.__addFilter(reg)
 
   let response1 = await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
 
   let response2 = await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
 
-  t.is(response1.data.id, 6868950)
+  t.is(response1.data.id, '7577')
   t.is(response1.__fromAxiosCache, undefined)
-  t.is(response2.data.id, 6868950)
+  t.is(response2.data.id, '7577')
   t.is(response2.__fromAxiosCache, true)
 })
 
@@ -212,16 +216,16 @@ test('ttl with maxCacheSize test', async t => {
     maxCacheSize: 1,
     ttl: 10000
   })
-  let reg = /users/
+  let reg = /employee/
   http.__addFilter(reg)
 
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
   t.is(http.__cacher.cacheMap.size, 1)
   await http({
-    url: 'http://www.404forest.com:3000/users/yyhappynice',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7578',
     method: 'get'
   })
   t.is(http.__cacher.cacheMap.size, 1)
@@ -229,7 +233,7 @@ test('ttl with maxCacheSize test', async t => {
   await pause(11000)
 
   t.falsy(http.__cacher.hasCache({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   }))
 
@@ -240,11 +244,11 @@ test('with options ignore header', async t => {
   let http = wrapper(axios, {
     excludeHeaders: true
   })
-  let reg = /users/
+  let reg = /employee/
   http.__addFilter(reg)
 
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get',
     headers: {
       'unique-prop': Math.random().toString()
@@ -252,7 +256,7 @@ test('with options ignore header', async t => {
   })
   t.is(http.__cacher.cacheMap.size, 1)
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get',
     headers: {
       'unique-prop': Math.random().toString()
@@ -265,11 +269,11 @@ test('with out options ignore header', async t => {
 
   let http = wrapper(axios, {})
 
-  let reg = /users/
+  let reg = /employee/
   http.__addFilter(reg)
 
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get',
     headers: {
       'unique-prop': Math.random().toString()
@@ -277,7 +281,7 @@ test('with out options ignore header', async t => {
   })
   t.is(http.__cacher.cacheMap.size, 1)
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get',
     headers: {
       'unique-prop': Math.random().toString()
@@ -289,7 +293,7 @@ test('with out options ignore header', async t => {
 test('clear cache', async t => {
 
   let http = wrapper(axios)
-  let reg = /users/
+  let reg = /employee/
   http.__addFilter(reg)
 
   t.is(http.__cacher.cacheMap.size, 0)
@@ -297,14 +301,14 @@ test('clear cache', async t => {
   t.is(http.__cacher.cacheMap.size, 0)
 
   await http({
-    url: 'http://www.404forest.com:3000/users/jin5354',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7577',
     method: 'get'
   })
 
   t.is(http.__cacher.cacheMap.size, 1)
 
   await http({
-    url: 'http://www.404forest.com:3000/users/yyhappynice',
+    url: 'http://dummy.restapiexample.com/api/v1/employee/7578',
     method: 'get'
   })
 
